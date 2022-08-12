6bf6eaea1de0cb852446d2be980dc1523753e044
jitendra kumar <jitendra199618@gmail.com>
2022-08-12 13:15:03 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      "./components/**/*.{vue,js}",
+      "./layouts/**/*.vue",
+      "./pages/**/*.vue",
+      "./plugins/**/*.{js,ts}",
+      "./nuxt.config.{js,ts}",
+    ],
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  };
